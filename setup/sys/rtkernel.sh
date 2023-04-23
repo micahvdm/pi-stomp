7823cb2b7b6e1bd2a9010489a4a4c34fe34aa72a
Micah Van de Merwe <micahvdm@Micahs-MacBook-Pro.local>
2023-04-23 23:42:21 +0200

    fix path error for kernel install

--- setup/sys/rtkernel.sh
+++ setup/sys/rtkernel.sh
@@ -4,7 +4,7 @@ set -x
 set -e
 
 sudo dpkg -r --force-depends raspberrypi-kernel
-sudo dpkg -i ./raspberrypi-kernel_1.20230405-1_arm64.deb
+sudo dpkg -i setup/sys/raspberrypi-kernel_1.20230405-1_arm64.deb
 
 #Turn off raspi-config service and set performance governor
 sudo rcconf --off raspi-config

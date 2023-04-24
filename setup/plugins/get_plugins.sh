0dfd849cbb6c64f5465de6b8924c1f63871c2e91
Micah Van de Merwe <micahvdm@Micahs-MacBook-Pro.local>
2023-04-24 11:22:38 +0200

    new kernel build

--- setup/plugins/get_plugins.sh
+++ setup/plugins/get_plugins.sh
@@ -17,6 +17,6 @@
 
 set -x
 
-wget https://www.treefallsound.com/downloads/lv2plugins.tar.gz
+wget -q https://www.treefallsound.com/downloads/lv2plugins.tar.gz
 tar -zxf lv2plugins.tar.gz -C $HOME
 rm lv2plugins.tar.gz

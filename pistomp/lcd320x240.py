ad71dd1bd96960fb3ca25f54c932c60e6d40f707
DivideCo <micahvdm@gmail.com>
2023-11-19 23:18:58 +0200

    remove arg1 arg2

--- pistomp/lcd320x240.py
+++ pistomp/lcd320x240.py
@@ -197,7 +197,7 @@ class Lcd(abstract_lcd.Lcd):
         if event == InputEvent.CLICK:
             self.draw_wifi_dialog(event, widget)
         elif event == InputEvent.LONG_CLICK:
-            self.handler.system_toggle_hotspot(arg1, arg2)
+            self.handler.system_toggle_hotspot()
 
     def draw_bypass_preference(self):
         pref = self.handler.settings.get_setting(Token.BYPASS)

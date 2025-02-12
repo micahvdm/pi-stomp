6d29092aab8b8ef899507b236c8c4223fdebf920
Micah John <micahvdm@gmail.com>
2025-02-12 09:25:43 +0200

    fix utils folder

--- util/modify_version.sh
+++ util/modify_version.sh
@@ -27,7 +27,7 @@ config_dir="$HOME/data/config"
 config_file="$config_dir/default_config.yml"
 hwfile="$config_dir/hardware-descriptor.json"
 
-template_dir="$HOME/pi-stomp/setup/config_templates"
+template_dir="$HOME/pi-stomp/util/config_templates"
 pistomp_orig_config_file="$template_dir/default_config_pistomp.yml"
 pistomp_core_config_file="$template_dir/default_config_3fs_2knob_exp.yml"
 pistomp_tre_config_file="$template_dir/default_config_pistomptre.yml"
